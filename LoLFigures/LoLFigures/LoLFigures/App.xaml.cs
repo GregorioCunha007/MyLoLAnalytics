668e0c7159a241e9cf6c3b41d86c464b5d64d8a2
GregorioCunha007 <40622@alunos.isel.ipl.pt>
2020-03-27 21:15:58 +0000

    Initializations. Showing profile image

---
+++ LoLFigures/LoLFigures/LoLFigures/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using RiotSimplify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LoLFigures
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+            var riot = new RiotApiClient("RGAPI-9df4f174-1ccc-4da6-ae88-0e167478c90b", "greggz");
+			MainPage = new LoLFigures.MainPage(riot);
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ LoLFigures/LoLFigures/LoLFigures/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using RiotSimplify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using RiotSimplify.Services;
+
+namespace LoLFigures
+{
+	public partial class MainPage : ContentPage
+	{
+        public UserAccountService UserService { get; set; }
+
+		public MainPage(RiotApiClient apiClient)
+		{
+			InitializeComponent();
+            UserService = apiClient;
+		}
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                var summonerIconPath = await UserService.GetSummonerIcon();
+
+                SummonerIcon.Source = ImageSource.FromUri(new Uri(summonerIconPath));
+
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }           
+        }
+    }
+}
